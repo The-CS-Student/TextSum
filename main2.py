29873edb432b3e17bd38a1c2bd32850bd60fd775
The-CS-Student <54126518+The-CS-Student@users.noreply.github.com>
2019-12-30 18:38:48 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,44 @@
+
+import re
+import nltk
+
+infinity = float('inf')
+def getsummary(text,lines):
+		
+		article_text = text
+		article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+		article_text = re.sub(r'\s+', ' ', article_text)
+		formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+		formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+		sentence_list = nltk.sent_tokenize(article_text)
+		stopwords = nltk.corpus.stopwords.words('english')
+		word_frequencies = {}
+		for word in nltk.word_tokenize(formatted_article_text):
+		    if word not in stopwords:
+		        if word not in word_frequencies.keys():
+		            word_frequencies[word] = 1
+		        else:
+		            word_frequencies[word] += 1
+		maximum_frequncy = max(word_frequencies.values())
+		for word in word_frequencies.keys():
+		    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+		sentence_scores = {}
+		for sent in sentence_list:
+		    for word in nltk.word_tokenize(sent.lower()):
+		        if word in word_frequencies.keys():
+		            if len(sent.split(' ')) < infinity:
+		                if sent not in sentence_scores.keys():
+		                    sentence_scores[sent] = word_frequencies[word]
+		                else:
+		                    sentence_scores[sent] += word_frequencies[word]
+		import heapq
+		summary_sentences = heapq.nlargest(lines, sentence_scores, key=sentence_scores.get)
+		summary = ' '.join(summary_sentences)
+		return summary
+def run():
+	
+	filename = "richardfeynman.txt"
+	txt = open(filename,"r").read()
+	summary = getsummary(txt,10)
+	print(summary)
+run()
